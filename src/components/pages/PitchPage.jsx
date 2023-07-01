56a1c32a7e5d6aced7e30d30c0eafa07426ebdaf
thePianoKid <gbraden.dev@gmail.com>
2023-07-01 15:20:01 -0400

    Added service images to pitch page

--- src/components/pages/PitchPage.jsx
+++ src/components/pages/PitchPage.jsx
@@ -5,6 +5,9 @@ import { styled } from "@mui/material/styles";
 import { properties } from "../../properties";
 import ThemedButton from "../ThemedButton";
 import { Link } from "react-router-dom";
+import service1Image from "../../resources/images/service1-img.jpeg";
+import service2Image from "../../resources/images/service2-img.jpeg";
+import service3Image from "../../resources/images/service3-img.jpeg";
 
 const SplashPageTitle = styled(Typography)(() => ({
   fontFamily: "Josefin Sans",
@@ -30,6 +33,7 @@ function PitchPage(props) {
       <Grid container spacing={2} alignItems="center" alignContent="center" justifyContent="center">
         <Grid item xs={4}>
             <SplashPageTitle sx={{ pt: 4, ml: 3 }}>{ properties.services.service1 }</SplashPageTitle>
+            <img src={service1Image} style={{ width: "70%", marginLeft: "25px", borderRadius: "5px" }} alt="Service 1" />
             <SplashPageBody sx={{ pt: 2, pb: 4, ml: 3 }}>
                 Lorem ipsum dolor sit amet, consectetur. Lorem ipsum.
             </SplashPageBody>
@@ -39,6 +43,7 @@ function PitchPage(props) {
         </Grid>
         <Grid item xs={4}>
             <SplashPageTitle sx={{ pt: 4, ml: 3 }}>{ properties.services.service2 }</SplashPageTitle>
+            <img src={service2Image} style={{ width: "80%", marginLeft: "25px", borderRadius: "5px" }} alt="Service 2" />
             <SplashPageBody sx={{ pt: 2, pb: 4, ml: 3 }}>
                 Lorem ipsum dolor sit amet, consectetur. Lorem ipsum.
             </SplashPageBody>
@@ -48,6 +53,7 @@ function PitchPage(props) {
         </Grid>
         <Grid item xs={4}>
             <SplashPageTitle sx={{ pt: 4, ml: 3 }}>{ properties.services.service3 }</SplashPageTitle>
+            <img src={service3Image} style={{ width: "80%", marginLeft: "25px", borderRadius: "5px" }} alt="Service 3" />
             <SplashPageBody sx={{ pt: 2, pb: 4, ml: 3 }}>
                 Lorem ipsum dolor sit amet, consectetur. Lorem ipsum.
             </SplashPageBody>
