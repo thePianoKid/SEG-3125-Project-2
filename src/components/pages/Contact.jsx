7b53218e30f285b49f57941559c5e42d0cc1679f
thePianoKid <gbraden.dev@gmail.com>
2023-07-01 15:54:57 -0400

    Added basic confirmation page

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../Navbar";
 import { Typography, TextField, Grid } from "@mui/material";
 import { styled } from "@mui/material/styles";
@@ -11,34 +11,55 @@ const SplashPageTitle = styled(Typography)(() => ({
   color: properties.style.primary.grey,
 }));
 
+const SplashPageBody = styled(Typography)(() => ({
+  fontSize: "18px",
+  color: properties.style.primary.grey,
+}));
+
 const Contact = () => {
+  const [showConfirmationPage, setShowConfirmationPage] = useState(false);
+
   return (
     <div>
-      <div>
-        <Navbar />
-
-        <SplashPageTitle sx={{ pt: 4, pb: 2, ml: 3 }}>Contact Steampunk</SplashPageTitle>
-        <Grid sx={{ pb: 3 }} container>
-          <TextField
-            sx={{ ml: 3, width: 400 }}
-            label="Enter your work email..."
-            variant="filled"
+      <Navbar />
+      {!showConfirmationPage ? (
+        <div>
+          <SplashPageTitle sx={{ pt: 4, pb: 2, ml: 3 }}>
+            Contact Steampunk
+          </SplashPageTitle>
+          <Grid sx={{ pb: 3 }} container>
+            <TextField
+              sx={{ ml: 3, width: 400 }}
+              label="Enter your work email..."
+              variant="filled"
+            />
+          </Grid>
+          <Grid sx={{ pb: 3 }}>
+            <TextField
+              sx={{ ml: 3, width: 600 }}
+              label="Questions or comments?"
+              variant="filled"
+              multiline
+              rows={5}
+            />
+          </Grid>
+          <ThemedButton
+            sx={{ ml: 3 }}
+            btnText="Submit"
+            onClick={() => setShowConfirmationPage(true)}
           />
-        </Grid>
-        <Grid sx={{ pb: 3 }}>
-          <TextField
-            sx={{ ml: 3, width: 600 }}
-            label="Questions or comments?"
-            variant="filled"
-            multiline
-            rows={5}
-          />
-        </Grid>
-        <ThemedButton
-          sx={{ ml: 3 }}
-          btnText="Submit"
-        />
-      </div>
+        </div>
+      ) : (
+        <div>
+          <SplashPageTitle sx={{ pt: 4, pb: 2, ml: 3 }}>
+            Submission Successful!
+          </SplashPageTitle>
+          <SplashPageBody sx={{ pt: 2, pb: 1, ml: 3 }}>
+            Thank you for your interest. We will get back to you in 1-2 business
+            days.
+          </SplashPageBody>
+        </div>
+      )}
     </div>
   );
 };
